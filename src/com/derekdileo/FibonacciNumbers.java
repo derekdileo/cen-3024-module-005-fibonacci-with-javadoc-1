8e5dd662189cd8b078ffb52bebe8d743fa765cf5
Derek DiLeo <derekfdileo@gmail.com>
2021-10-02 15:01:48 -0400

    Create HashMaps of type <Integer, Long> to store position and time for each iteration. These key/values will later be used to plot to a line graph

--- src/com/derekdileo/FibonacciNumbers.java
+++ src/com/derekdileo/FibonacciNumbers.java
@@ -1,5 +1,10 @@
 package com.derekdileo;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 /** Class to calculate fibonacci numbers with main method to execute and compare iterative and recursive execution times
  * @author Derek DiLeo
  */
@@ -13,6 +18,12 @@ public class FibonacciNumbers {
      */
     public static int value, position;
 
+    /** Map<Integer, Long> to store positions and times for each method call
+      */
+
+    public static Map<Integer, Long> recursiveTimes = new LinkedHashMap<>();
+    public static Map<Integer, Long> iterativeTimes = new LinkedHashMap<>();
+
     /** Main method to run the methods created to return fibonacci number of nth position
       * @param args mandatory String[] array for console line arguments of data type String
      */
@@ -25,31 +36,33 @@ public class FibonacciNumbers {
         // [to compare and plot differences in execution times]
 
         for (int i = 0; i < position; i++) {
-            printRecursive(i);
-            printIterative(i);
+            recursiveTimes.put(i, printRecursive(i));
+            iterativeTimes.put(i, printIterative(i));
         }
     }
 
     /** Method to call fibIterative(), record execution time and print results
       * @param position is the nth fibonacci number requested (zero-index)
      */
-    private static void printIterative(int position) {
+    private static long printIterative(int position) {
         startTime = System.nanoTime();
         value = fibIterative(position);
         endTime = System.nanoTime();
         runTime = endTime - startTime;
         System.out.println("Iterative runtime for: " + position + " is: " + runTime + " nanoSeconds. Value is: " + value);
+        return runTime;
     }
 
     /** Method to call fibRecursive(), record execution time and print results
      * @param position is the nth fibonacci number requested (zero-index)
      */
-    private static void printRecursive(int position) {
+    private static long printRecursive(int position) {
         startTime = System.nanoTime();
         value = fibRecursive(position);
         endTime = System.nanoTime();
         runTime = endTime - startTime;
         System.out.println("Recursive runtime for: " + position + " is: " + runTime + " nanoSeconds. Value is: " + value);
+        return runTime;
     }
 
     /** Method uses a for loop to iterate through fibonacci numbers and return value at nth position.
